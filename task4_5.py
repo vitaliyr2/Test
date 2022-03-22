42cd92d87496785a4db1017d70a162e7090c071c
vitaliyr2 <vitaliyr@bk.ru>
2022-03-22 15:09:51 +0300

    0

---
+++ task4_5.py
@@ -0,0 +1,12 @@
+command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
+command2 = 'switchport trunk allowed vlan 1,3,8,9'
+
+x=(command1[command1.find("vlan")+5:])
+x1=(set((x.split(","))))
+
+y=(command2[command2.find("vlan")+5:])
+y1=(set((y.split(","))))
+
+z=list(x1.union(y1))
+print(z)
+print(z)
